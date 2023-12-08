483100fea6a84238431f76f75122027f041f7135
Red-Souls <92220712+Red-Souls@users.noreply.github.com>
2023-12-08 13:41:59 +0700

    Update settings.py
    'App'

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(d1^k4*d41nh!$zkiq$1+@h^!ei)xz)%8n%_*2be0rmwp4_=@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
